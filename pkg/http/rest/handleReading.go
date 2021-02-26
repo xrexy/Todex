93d372a4cc923dd64c8c5942e324a1e547d6243c
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 22:03:33 +0200

    Added index handler

--- pkg/http/rest/handleReading.go
+++ pkg/http/rest/handleReading.go
@@ -2,11 +2,33 @@ package rest
 
 import (
 	"encoding/json"
+	"fmt"
+	"html/template"
 	"net/http"
+	"os"
 )
+
+// SimplePage is
+type SimplePage struct {
+	Title  string
+	Author string
+}
 
 func welcomeHandler() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode("niggers were a mistake")
+		json.NewEncoder(w).Encode("successfully linked")
+	}
+}
+
+func indexHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		p := SimplePage{Title: "Why rexy is the best", Author: "Rexy"}
+		tpl, err := template.ParseFiles("./pkg/http/rest/templates/index.html")
+		if err != nil {
+			fmt.Println("Couldn't parse html for index file! \n\n > Error:\n", err)
+			os.Exit(http.StatusNotFound)
+		} else {
+			tpl.Execute(w, p)
+		}
 	}
 }
--- pkg/http/rest/handler.go
+++ pkg/http/rest/handler.go
@@ -1,10 +1,14 @@
 package rest
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+)
 
 // InitHandlers is intializing all handlers
 func InitHandlers() *mux.Router {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/", indexHandler())
 
 	router.HandleFunc("/help", welcomeHandler())
 	return router
